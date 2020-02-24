517c28ff27d37f1090be422c15a7c66127a3f26d
Benjamin Phillips <bdphillips013@gmail.com>
2020-02-23 22:27:19 -0500

    Test now tests the correct class

--- src/test/java/TestMergeSort.java
+++ src/test/java/TestMergeSort.java
@@ -3,6 +3,6 @@ import org.junit.jupiter.api.BeforeEach;
 public class TestMergeSort extends TestSort {
     @BeforeEach
     public void setup() {
-        sort = new InsertionSort();
+        sort = new MergeSort();
     }
 }
